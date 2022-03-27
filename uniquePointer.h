61b47e596adbf8912e0231ba597241f5318b9541
AndrewS-GH <66842882+AndrewS-GH@users.noreply.github.com>
2022-03-27 11:47:35 -0400

    Update uniquePointer.h

--- uniquePointer.h
+++ uniquePointer.h
@@ -1,3 +1,7 @@
+// a rough unique pointer implementation
+// templated class to manage memory of dynamically created objects
+// no two unique pointers can point to the same object
+
 #pragma once
 
 template<class T>
@@ -10,25 +14,26 @@ public:
 	UniquePointer(T* data = nullptr)
 		: m_data{ data }
 	{
-		
 	}
 
 	~UniquePointer()
 	{
-		delete m_data;	
+		if (m_data) // delete if not nullptr
+			delete m_data;
 	}
 
 	// no copy or assignment
 	UniquePointer(const UniquePointer& up) = delete;
 	UniquePointer& operator=(const UniquePointer& up) = delete;
 
-	// Move
+	// Move Constructor
 	UniquePointer(UniquePointer&& up) noexcept
 		: m_data{ up.m_data }
 	{
-		up.m_data = nullptr;
+		up.m_data = nullptr; // ownership transfer, set original to nullptr
 	}
 
+	// R-Value Ref assignment
 	UniquePointer& operator=(UniquePointer&& up)
 	{
 		if (&up == this)
@@ -45,48 +50,51 @@ public:
 	T& operator*() const { return *m_data; }
 	T* operator->() const { return m_data; }
 
-	// Modifiers
+	// returns a pointer to managed object, releases ownership
 	T* release()
 	{
 		T* temp{ m_data };
 		m_data = nullptr;
 		return temp;
 	}
-	
+
+	// replaces the managed object
 	void reset(T* data)
 	{
-		if (m_data)
+		if (m_data) // delete if not nullptr
 			delete m_data;
 		m_data = data;
 	}
 
+	// swaps the managed objects
 	void swap(UniquePointer& up)
 	{
 		T* temp{ up.release() };
-		
-		up = std::move(*this);
+
+		up = std::move(*this); // calls R-Value Reference assignment
 		m_data = temp;
 
 	}
-	
+
 	T* get() const { return m_data; }
 	operator bool() const { return static_cast<bool>(m_data); }
 
 	friend bool operator==(const UniquePointer& up1, const UniquePointer& up2) { return up1.get() == up2.get(); }
 	friend bool operator!=(const UniquePointer& up1, const UniquePointer& up2) { return up1.get() != up2.get(); }
-	
 
-	template<typename T>
-	friend UniquePointer& make_unique(T* data)
-	{
-		return UniquePointer<T>(data);
-	}
-
-	friend std::ostream& operator<<(std::ostream& out, const UniquePointer& up)
-	{
-		out << up.get();
-		return out;
-	}
 
 };
 
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const UniquePointer<T>& up)
+{
+	out << up.get();
+	return out;
+}
+
+
+template<typename T>
+UniquePointer<T> make_unique(T&& data)
+{
+	return UniquePointer<T>(new T(data));
+}
